d43ba9fa6296a061f9955ce08507705b5d55e530
Es7evam <estevam.arantes@gmail.com>
2020-08-18 01:21:18 -0300

    Temporary fix for the /now command

--- app.py
+++ app.py
@@ -274,7 +274,15 @@ class App:
 		now = int(time.time())
 		daysAgo = now - 300000
 
-		f = urllib.request.urlopen('https://ctftime.org/api/v1/events/?limit=5&start={}'.format(daysAgo))
+		reqHeader = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    	'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    	'Accept-Encoding': 'none',
+    	'Accept-Language': 'en-US,en;q=0.8',
+		'Connection': 'keep-alive'}
+
+		req = urllib.request.Request('https://ctftime.org/api/v1/events/?limit=5&start={}'.format(daysAgo), headers=reqHeader)
+		f = urllib.request.urlopen(req)
 		l = json.load(f)
 		newL = []
 		genstr = '%a, %B %d, %Y %H:%M UTC '	#
