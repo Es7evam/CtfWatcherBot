3d3ba2b6f3f53202f42842aac0fa0b7cbe214d21
Es7evam <estevam.arantes@gmail.com>
2020-08-18 07:47:53 -0300

    Add event scrapping for the scoreboard

--- eventScrapper.py
+++ eventScrapper.py
@@ -2,8 +2,8 @@ from requests import get
 from bs4 import BeautifulSoup
 from collections import defaultdict
 
-# Returns the list of participating teams in a subscribers list
-def getEventParticipants(eventId):
+# Makes a request to a url and returns a beautifulSoup object
+def makeRequest(url):
     reqHeader = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -11,11 +11,20 @@ def getEventParticipants(eventId):
     'Accept-Language': 'en-US,en;q=0.8',
     'Connection': 'keep-alive'}
 
-    url = 'https://ctftime.org/event/{}'.format(eventId)
-    req = get(url, headers=reqHeader, timeout=5)
+    try:
+        req = get(url, headers=reqHeader, timeout = 5)
+    except:
+        print("Request to url {} failed!".format(url))
 
-    # HTML Parsing
     html = BeautifulSoup(req.content, 'html.parser')
+
+    return html
+
+# Returns the list of participating teams in a subscribers list
+def getEventParticipants(eventId):
+    url = 'https://ctftime.org/event/{}'.format(eventId)
+
+    html = makeRequest(url)
     teams = html.findAll('td')
 
     # Searching for the teams in the event
@@ -33,4 +42,23 @@ def listToDict(LTeamSubscribers):
         teamDict[int(k)] = v
     
     return teamDict
+
+def getRating(eventId):
+    url = 'https://ctftime.org/event/{}'.format(eventId)
+    html = makeRequest(url)
+
+    # Gets the team list 
+    teamList = html.findAll('tr')[1:]
+
+    scoreboard = []
+    for team in teamList:
+        teamInfo = team.findAll('td')
+        place = teamInfo[1].text
+        teamName = teamInfo[2].text
+        #points = teamInfo[3].text
+        rating = teamInfo[4].text
+
+        scoreboard.append([teamName, place, rating])
+
+    return scoreboard
 
